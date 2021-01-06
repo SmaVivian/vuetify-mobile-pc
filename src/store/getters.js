5bb047ecdd7c05515d476b030f60ada2d081713b
SmaVivian <vivi163abc@163.com>
2021-01-06 12:24:15 +0800

    清空项目

--- src/store/getters.js
+++
@@ -1,6 +0,0 @@
-const getters = {
-  headerShow: state => state.app.headerShow,
-  defaultImg: state => state.app.defaultImg,
-  defaultHeadImg: state => state.app.defaultHeadImg
-}
-export default getters
